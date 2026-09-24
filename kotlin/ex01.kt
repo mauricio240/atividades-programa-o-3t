02a8ea9b6ccbfd94177a000a1d18e6abb4cbbd02
mauricio240 <mauricio.maristany.junior@escola.pr.gov.br>
2026-09-24 11:41:20 -0300

    Create ex01.kt

---
+++ kotlin/ex01.kt
@@ -0,0 +1,9 @@
+  fun main() {'
+      printlm("==========================")
+      println("MEU PRIMEIRO PROGRAMA")
+      println("==========================")
+      println("Nome: Aluno exemplo")
+      println("Turma: DS3")
+      println("Escola: Alberto Gomes Veiga")
+      println("Bem-vindo ao Kotlin!")
+}
